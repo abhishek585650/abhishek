257849492d04ade9fd4a12195a7665801588cae9
abhishek singh <singhabhishek9425@.gmail.com>
2023-12-06 21:20:19 +0530

    first commit

---
+++ html.html/project/script.js
@@ -0,0 +1,7 @@
+// let a=parseInt(prompt("Enter the no."));
+//         let b=parseInt(prompt("Enter a no."));
+//         let sum;
+//         sum=a+b;
+//         console.log(sum);
+
+
---
+++ p1.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"my name abhishek singh ";
+    return 0;
+}
